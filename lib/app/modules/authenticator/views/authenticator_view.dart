c1c0eefe020ddf8d67191d2a957024c24a0bacdd
mds115577 <70107377+mds115577@users.noreply.github.com>
2022-10-29 01:48:58 +0530

    ui completed

--- lib/app/modules/authenticator/views/authenticator_view.dart
+++ lib/app/modules/authenticator/views/authenticator_view.dart
@@ -8,26 +8,20 @@ import 'package:get/get.dart';
 import '../controllers/authenticator_controller.dart';
 
 class AuthenticatorView extends GetView<AuthenticatorController> {
-  final AuthenticatorController _authenticatorController =
-      Get.put(AuthenticatorController());
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _authenticatorController.coneect(),
-        builder: (condtext, snapshot) {
-          return StreamBuilder<User?>(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Container();
-                } else if (snapshot.hasData) {
-                  return HomeView();
-                } else {
-                  return SignInView();
-                }
-              });
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Container();
+          } else if (snapshot.hasData) {
+            return HomeView();
+          } else {
+            return SignInView();
+          }
         });
   }
 }
